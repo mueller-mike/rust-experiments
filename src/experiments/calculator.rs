41d510f946d23a24a0d26a51e56778b785966bf3
mueller-mike <mike.mueller28@outlook.com>
2025-06-13 14:17:28 +0200

    Adds calculator.rs and implements it into main.rs and mod.rs.

---
+++ src/experiments/calculator.rs
@@ -0,0 +1,98 @@
+use std::io;
+use std::thread;
+use std::time::Duration;
+
+pub fn run() {
+    thread::sleep(Duration::from_millis(500));
+    println!("[SYSTEM] Starting Calculator");
+    thread::sleep(Duration::from_secs(1));
+    println!("Welcome to Calculator!");
+    println!("Available Operations:");
+    println!("1. Addition");
+    println!("2. Subtract");
+    println!("3. Multiply");
+    println!("4. Divide");
+    println!("5. Quit");
+
+    loop {
+        println!("Please enter your desired operation:");
+        
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+        let command = command.trim().to_lowercase();
+
+        match command.as_str() {
+            "quit" => {
+                thread::sleep(Duration::from_millis(500));
+                println!("[SYSTEM] Calculator Finished Execution");
+                break;
+            }
+            "addition" | "1" | "subtract" | "2" |"multiply" | "3" | "divide" | "4" => {
+                println!("Please enter x value:");
+                let mut x = String::new();
+                io::stdin().read_line(&mut x).expect("Failed to read line");
+                let x: i32 = match x.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("Invalid number for x.");
+                        continue;
+                    }
+                };
+
+                println!("Please enter y value:");
+                let mut y = String::new();
+                io::stdin().read_line(&mut y).expect("Failed to read line");
+                let y: i32 = match y.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("Invalid number for y.");
+                        continue;
+                    }
+                };
+
+                match command.as_str() {
+                    "addition" | "1" => {
+                        addition(x, y);
+                    }
+                    "subtract" | "2" => {
+                        subtract(x, y);
+                    }
+                    "multiply" | "3" => {
+                        multiply(x, y);
+                    }
+                    "divide" | "4" => {
+                        divide(x, y);
+                    }
+                    _ => {
+                        println!("Invalid command.");
+                    }
+                }
+            }
+            _ => {
+                println!("Unknown command: '{}'", command);
+            }
+        }
+    }
+}
+
+fn addition(x: i32, y: i32) {
+    let z: i32 = x + y;
+    println!("{} + {} = {}", x, y, z);
+}
+
+fn subtract(x: i32, y: i32) {
+    let z: i32 = x - y;
+    println!("{} - {} = {}", x, y, z);
+}
+
+fn multiply(x: i32, y: i32) {
+    let z: i32 = x * y;
+    println!("{} * {} = {}", x, y, z);
+}
+
+fn divide(x: i32, y: i32) {
+    let z: f32 = x as f32 / y as f32;
+    println!("{} / {} = {}", x, y, z);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 mod experiments;
 
-use experiments::{hello};
+use experiments::calculator;
+use experiments::hello;
 
 use std::io;
 
 fn main() {
+    
+    println!("[SYSTEM] Welcome to the Rust-Experiments program!");
+    
     loop {
-        println!("[SYSTEM] Welcome to the Experiments program!");
-        println!("[SYSTEM] 1. Hello");
-        println!("[SYSTEM] 2. Exit");
-        println!("[SYSTEM] Please choose a program by entering the corresponding number or name:");
-
+        print_available_programs();
+        
         let mut program_selection = String::new();
-        io::stdin().read_line(&mut program_selection).expect("Failed to read input");
-        let program_selection = program_selection.trim();
+        io::stdin()
+            .read_line(&mut program_selection)
+            .expect("Failed to read input");
+        let program_selection = program_selection.trim().to_lowercase();
 
-        match program_selection {
-            "1" => hello::run(),
-            "2" => {
-                println!("[SYSTEM] Goodbye!");
+        match program_selection.as_str() {
+            "1" | "hello" => hello::run(),
+            "2" | "calculator" => calculator::run(),
+            "3" | "quit" => {
+                println!("[SYSTEM] Rust-Experiments finished execution!");
                 break;
             }
+            "help" => print_available_programs(),
             _ => println!("[SYSTEM] Invalid choice."),
         }
     }
+}
+
+fn print_available_programs() {
+    println!("[SYSTEM] Available programs:");
+    println!("[SYSTEM] 1. Hello");
+    println!("[SYSTEM] 2. Calculator");
+    println!("[SYSTEM] 3. Quit");
+    println!("[SYSTEM] Please choose a program by entering the corresponding number or name:");
 }
